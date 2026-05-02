a9b9b242a5e0b5ed9fcbbdae02e013034a20156e
Klark-cyber <jamshidshukuraliyev8066@gmail.com>
2026-05-02 17:08:50 +0900

    feat: develop homepage components

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -3,15 +3,15 @@ import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+const AgentList: NextPage = () => {
+  
   const [title, setTitle] = useState<string>("hello");
 
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">AGENT LIST</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(AgentList);
--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -3,15 +3,15 @@ import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+const CS: NextPage = () => {
+  
   const [title, setTitle] = useState<string>("hello");
 
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">CS</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
